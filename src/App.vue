8db45e6448e9dd2ed347be7bcb01cc4afe67e526
olalekanteeblaze <olalekanteeblaze.com>
2020-11-16 10:32:07 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <CompletedTodos></CompletedTodos>
+    <GetTodo></GetTodo>
+    <CurrentTodos></CurrentTodos>
+  </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GetTodo from './components/GetTodo.vue'
+import CurrentTodos from './components/CurrentTodo.vue'
+import CompletedTodos from './components/CompletedTodos.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GetTodo,
+    CurrentTodos,
+    CompletedTodos
   }
 }
 </script>
@@ -17,10 +25,10 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  /* -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+// import Vue from 'vue';
+// import Vuex from 'vuex';
+import store from './store/store'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// new Vue({
+//     el: '#app',
+//     store,
+//     render: h => h(App)
+// })
+// createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(store)
+app.mount('#app')
